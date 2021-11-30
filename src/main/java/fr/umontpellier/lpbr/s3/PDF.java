62f2364a265856a606f9ad3d3937d2b954e114b4
Loulou <louispironneau69440@gmail.com>
2021-11-30 20:46:27 +0100

    Créations PDF pour round résulat

--- src/main/java/fr/umontpellier/lpbr/s3/PDF.java
+++ src/main/java/fr/umontpellier/lpbr/s3/PDF.java
@@ -16,22 +16,66 @@ import javafx.scene.control.Cell;
 
 public class PDF {
 
-    public static void roundCreatePDF(ArrayList<Partie> partieListe){
+    public static void resultatRoundCreatePDF(ArrayList<Partie> partieListe, Tournoi t){
         Document document = new Document();
         int nbPartie = partieListe.size();
         try {
-            PdfPTable table = new PdfPTable(8); // création du nombre de colone
+            PdfPTable table = new PdfPTable(6); // création du nombre de colone
             table.setWidthPercentage(100); //Width 100%
             table.setSpacingBefore(10f); //Space before table
             table.setSpacingAfter(10f); //Space after table
 
-             // noramalement ça gére la largeur des colones
+             // ajoue des noms des colums pour l'impression
+            PdfPCell cell1= new PdfPCell(new Paragraph("table"));
+            table.addCell(cell1);
 
-            int compteur = 1;
+            PdfPCell cell2= new PdfPCell(new Paragraph("pts"));
+            table.addCell(cell2);
+
+            PdfPCell cell3= new PdfPCell(new Paragraph("blanc"));
+            table.addCell(cell3);
+
+
+
+            PdfPCell cell5= new PdfPCell(new Paragraph("res"));
+            table.addCell(cell5);
+
+            PdfPCell cell6= new PdfPCell(new Paragraph("Noir"));
+            table.addCell(cell6);
+
+
+
+            PdfPCell cell8= new PdfPCell(new Paragraph("pts"));
+            table.addCell(cell8);
+
+
+
 
             for (Partie p : partieListe ) {
-                PdfPCell cell= new PdfPCell(new Paragraph(p.getId()));
-                table.addCell(cell);
+                PdfPCell nbTable= new PdfPCell(new Paragraph(p.getTable()));// début de la nouvelle ligne
+                table.addCell(nbTable); //ajoue de la cellule au tableau
+
+                PdfPCell ptsBlanc = new PdfPCell(new Paragraph((int)p.getJoueur_blanc().nbPoint(t,p.getNumRonde())));
+                table.addCell(ptsBlanc);
+
+                PdfPCell nomPrenomEloBlanc = new PdfPCell(new Paragraph(p.getJoueur_blanc().toString()));
+                table.addCell(nomPrenomEloBlanc);
+
+
+                PdfPCell resultat = new PdfPCell(new Paragraph(p.resultatToString()));
+                table.addCell(resultat);
+
+                PdfPCell nomPrenomEloNoir = new PdfPCell(new Paragraph(p.getJoueur_noir().toString()));
+                table.addCell(nomPrenomEloNoir);
+
+
+                PdfPCell ptsNoir = new PdfPCell(new Paragraph((int)p.getJoueur_noir().nbPoint(t,p.getNumRonde())));
+                table.addCell(ptsNoir);
+
+
+
+
+
             }
 
 
